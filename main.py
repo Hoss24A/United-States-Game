84e89a8d6be92c61ee681bd5b33d58b1acdd610f
Hossam Asad <hossamasad2004@gmail.com>
2025-01-14 16:26:34 -0500

    added some comments

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import turtle
 import pandas
 
+# build the screen
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# read all the states including the X and Y axis
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
